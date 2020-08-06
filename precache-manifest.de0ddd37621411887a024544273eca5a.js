a9d1801abdc86cb2a59ea441e09491796705ca6b
KellyEgesa <kelly.egesa@gmail.com>
2020-08-06 20:45:05 +0300

    Updates

---
+++ precache-manifest.de0ddd37621411887a024544273eca5a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fecd66c4bc874b89f83f277b49d332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1cdc5d83d693737c02d",
+    "url": "/static/css/2.cb7b0f97.chunk.css"
+  },
+  {
+    "revision": "079c0ee5f9ab5072169e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d1cdc5d83d693737c02d",
+    "url": "/static/js/2.3fb2ccd7.chunk.js"
+  },
+  {
+    "revision": "2dd6b9d260e8a37f9a3fdee3e85e17aa",
+    "url": "/static/js/2.3fb2ccd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079c0ee5f9ab5072169e",
+    "url": "/static/js/main.d005fd7d.chunk.js"
+  },
+  {
+    "revision": "2656bd2bd06e49fc5d23",
+    "url": "/static/js/runtime-main.fa9dfc47.js"
+  },
+  {
+    "revision": "773e1b91145c6b6f81595dab1af50adc",
+    "url": "/static/media/IMG-5355.773e1b91.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
